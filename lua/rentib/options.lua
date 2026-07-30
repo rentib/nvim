c7cf9dd024bb779b28ce9e9d8e0807ceb4a857b8
Rentib <sbitner420@tutanota.com>
2026-07-30 09:45:53 +0200

    feat(options): simplify

--- lua/rentib/options.lua
+++ lua/rentib/options.lua
@@ -1,9 +1,4 @@
-vim.g.vimsyn_embed         = "l"  -- highlight lua in vimscript
 vim.g.c_syntax_for_h       = true -- .h files are c not cpp
-
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
 
 vim.opt.filetype.plugin    = true
 vim.opt.filetype.indent    = true
@@ -24,15 +19,12 @@ vim.o.confirm    = true
 vim.o.updatetime = 250
 
 vim.opt.completeopt:append({ "menuone", "preview", "popup", "fuzzy", "noinsert", "noselect" })
-vim.o.wildmenu = true
-vim.opt.wildignore:append({ "*.o", "__pycache__", "*.pyc", "*pycache", "*.jpg", "*.png", "*.gif", "*.pdf" })
+vim.opt.wildignore:append({ "*.o" })
 vim.o.wildignorecase = true
 vim.o.wildmode       = "longest:full"
-vim.o.wildoptions    = "pum"
 
 vim.o.laststatus     = 0
 vim.o.showtabline    = 1
-vim.o.tabline        = ""
 vim.o.ruler          = true
 vim.o.showmode       = true
 vim.o.showcmd        = true
@@ -54,36 +46,26 @@ vim.o.inccommand     = "split"
 
 vim.o.autoindent     = true
 vim.o.smartindent    = true
-vim.o.cindent        = true
 vim.o.shiftwidth     = 4
 vim.o.tabstop        = 4
 vim.o.softtabstop    = 4
-vim.o.expandtab      = true
 vim.o.shiftround     = true
-
-vim.o.clipboard      = "unnamedplus"
-
-vim.o.mouse          = "nvi"
-vim.o.mousemoveevent = false
-vim.o.mousemodel     = "popup"
+vim.o.expandtab      = true
 
 vim.opt.shortmess:append("c")
 
 vim.o.textwidth = 79
-vim.o.linebreak = true
 vim.o.wrap      = false
-
 vim.cmd [[ au FileType * setlocal formatoptions=crqn1]jp ]] -- https://stackoverflow.com/questions/28375119/how-to-override-options-set-by-ftplugins-in-vim
--- o.formatoptions = o.formatoptions + "crqn1]jp" - "to/wa2vblmMB"
 
 vim.o.sidescroll    = 1
 vim.o.scrolloff     = 7
 vim.o.sidescrolloff = 8
 
 vim.o.virtualedit   = "block"
-vim.o.magic         = true
-vim.o.foldmethod    = "marker"
 vim.cmd [[ au FileType * setlocal foldmethod=marker ]]
-vim.o.nrformats     = "bin,hex"
 
 vim.o.termguicolors = true
+
+vim.o.clipboard      = "unnamedplus"
+vim.o.mouse          = "n"
