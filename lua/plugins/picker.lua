2e35febdf73e8978a7f02e86723578afcb3ef67d
Rentib <sbitner420@tutanota.com>
2026-07-30 15:20:27 +0200

    feat(picker): fix cursor blink, use builtin command
    Removed blinking cursor, by setting guicursor.
    Changed git files fallback to builtin vcs_files command.

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -2,33 +2,18 @@ return {
     "ibhagwan/fzf-lua",
     config = function()
         local fzf = require("fzf-lua")
+        local m   = require("keymap")
 
         fzf.setup({ "fzf-vim" })
 
-        local is_inside_work_tree = {}
-        local git_files_fallback = function()
-            local opts = {}
-
-            local cwd = vim.fn.getcwd()
-            if is_inside_work_tree[cwd] == nil then
-                vim.fn.system("git rev-parse --is-inside-work-tree")
-                is_inside_work_tree[cwd] = vim.v.shell_error == 0
-            end
-
-            if is_inside_work_tree[cwd] then
-                fzf.git_files(opts)
-            else
-                fzf.files(opts)
-            end
-        end
-
-        local m = require("keymap")
-
-        m.n("<c-p>", git_files_fallback)
+        m.n("<c-p>",      fzf.vcs_files)
         m.n("<leader>ff", fzf.files)
         m.n("<leader>fg", fzf.live_grep_native)
-        m.n("<leader>fh", fzf.helptags)
-        m.n("<leader>fm", fzf.manpages)
         m.n("<leader>fd", fzf.diagnostics_workspace)
+        m.n("<leader>fm", fzf.manpages)
+        m.n("<leader>fh", fzf.helptags)
+
+        -- disable blinking cursor
+        vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:block"
     end
 }
