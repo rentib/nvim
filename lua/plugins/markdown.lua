38f8ef26c9846b76baf15eb650072b7a4beba118
rentib <sbitner420@tutanota.com>
2026-08-19 15:27:26 +0200

    feat(markdown): remove preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,10 +7,4 @@ return {
             "nvim-tree/nvim-web-devicons"
         },
     },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end, -- on first run call by hand
-    },
 }
